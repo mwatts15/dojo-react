fa39bfc715cf52654dee672432d684728550280e
Mark Watts <watts.mark2015@gmail.com>
2019-12-22 22:06:41 -0600

    Initial commit with React+Dojo

---
+++ src/Clock.jsx
@@ -0,0 +1,15 @@
+define([
+    'react'
+], function(
+    React
+) {
+    function Clock(props) {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {props.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+    return Clock;
+});
